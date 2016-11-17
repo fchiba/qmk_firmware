ea29051528dfc1fcc0b052c98ac64fdba26b944e
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-11-18 00:16:17 +0900

    Remove unnecessary assign

--- keyboards/ergodox/keymaps/fchiba/keymap.c
+++ keyboards/ergodox/keymaps/fchiba/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL,        LT(SYMB,KC_A), KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSFT,         LT(MDIA,KC_Z), KC_X,     KC_C,   KC_V,   KC_B,   KC_NO,
         KC_NO,           KC_LALT,       KC_LGUI,  KC_NO,  KC_NO,
-                                                  ALT_T(KC_APP),  KC_NO,
+                                                          KC_NO,  KC_NO,
                                                                   KC_NO,
                                                   KC_SPC,KC_BSPC, KC_LANG2,
         // right hand
