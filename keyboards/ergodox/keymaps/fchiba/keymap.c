dc35b373177204d0fd8849643e369d92688edd07
Fumiya Chiba <fumiya.chiba@nifty.com>
2018-10-12 08:33:09 +0900

    update

--- keyboards/ergodox/keymaps/fchiba/keymap.c
+++ keyboards/ergodox/keymaps/fchiba/keymap.c
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,          KC_1,          KC_2,     KC_3,   KC_4,   KC_5,   KC_NO,
-        KC_TAB,          KC_Q,          KC_W,     KC_E,   KC_R,   KC_T,   KC_Y,
-        KC_LCTRL,        LT(SYMB,KC_A), KC_S,     KC_D,   KC_F,   KC_G,
-        KC_LSFT,         LT(MDIA,KC_Z), KC_X,     KC_C,   KC_V,   KC_B,   KC_NO,
-        KC_NO,           KC_LALT,       KC_LGUI,  KC_NO,  KC_NO,
-                                                          KC_NO,  KC_NO,
-                                                                  KC_NO,
-                                                  KC_SPC,KC_BSPC, KC_LANG2,
+        KC_ESC,          KC_1,          KC_2,     KC_3,     KC_4,   KC_5,   KC_NO,
+        KC_TAB,          KC_Q,          KC_W,     KC_E,     KC_R,   KC_T,   KC_Y,
+        KC_LCTRL,        LT(SYMB,KC_A), KC_S,     KC_D,     KC_F,   KC_G,
+        KC_LSFT,         KC_Z,          KC_X,     KC_C,     KC_V,   KC_B,   KC_NO,
+        KC_NO,           KC_LALT,       KC_LGUI,  KC_LALT,  KC_NO,
+                                                            KC_NO,  KC_NO,
+                                                                    KC_NO,
+                                                  KC_SPC,  KC_BSPC, KC_LANG2,
         // right hand
              KC_NO,           KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
              LT(KC_FN2,KC_T), KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_JAT,
